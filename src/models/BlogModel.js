d221a69a2420d70a74c674b76dcb270670535d1e
Nicholas Rowe <148307107+NRowe1@users.noreply.github.com>
2024-06-20 20:54:59 +1000

    setup models

---
+++ src/models/BlogModel.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const blogSchema = mongoose.Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    content: {
+        type: String,
+        require: true
+    },
+    author: {
+        type: String,
+        require: true
+    },
+    likes: {
+        type: [String]
+    },
+    headerImage: {
+        type: String,
+        require: false
+    },
+    tags: {
+        type: [String],
+        required: true
+    },
+    tags: {
+        type: [String],
+        enum: ["life", "travel", "photography", "coding"],
+        required: true
+    },
+
+    editHistory: {
+        type: [{user: String, timestamp: Date}],
+        required: false
+    }
+},
+{
+    timestapms: true
+})
+
+const BlogModel = mongoose.model("Blog", blogSchema);
+
+module.exports = {
+    BlogModel
+}
